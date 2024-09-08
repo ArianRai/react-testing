1259591aa5405a78224e6d150fd0121bde9bb461
ArianRai <arianraiesteban_20@hotmail.com>
2024-09-08 22:02:19 +0200

    finished

--- 01-react-fetch-prom-stat/src/App.jsx
+++ 01-react-fetch-prom-stat/src/App.jsx
@@ -1,3 +1,31 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+const ENDPOINT_RANDOM_CAT_FACT = 'https://catfact.ninja/fact'
+const CAT_PREFIX_URL = 'https://cataas.com'
+
 export function App() {
-	return <h1>Aiuda</h1>
+	const [fact, setFact] = useState()
+	const [imgUrl, setImgUrl] = useState()
+
+	useEffect(() => {
+		fetch(ENDPOINT_RANDOM_CAT_FACT)
+			.then(response => response.json())
+			.then(({ fact }) => setFact(fact))
+	}, [])
+
+	useEffect(() => {
+		if (!fact) return
+
+		const threeFirstWords = fact.split(' ', 3).join(' ')
+		fetch(`${CAT_PREFIX_URL}/cat/says/${threeFirstWords}`).then(({ url }) => setImgUrl(url))
+	}, [fact])
+
+	return (
+		<main>
+			<h1>Aiuda</h1>
+			{fact && <p>{fact}</p>}
+			{imgUrl && <img src={imgUrl} alt='gatito' />}
+		</main>
+	)
 }
